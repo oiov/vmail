e5e7b75931550d4a489d9081fe6e37f47945ea4c
oiov <stacystayfine718@gmail.com>
2024-04-10 17:27:01 +0800

    fix: converted to lowercase(#4)

--- apps/remix/lib/hooks/utlis.ts
+++ apps/remix/lib/hooks/utlis.ts
@@ -1,4 +1,4 @@
 export function getRandomCharacter(): string {
-  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.";
+  const characters = "abcdefghijklmnopqrstuvwxyz.";
   return characters.charAt(Math.floor(Math.random() * characters.length));
 }
