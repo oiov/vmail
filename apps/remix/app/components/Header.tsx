d2224d7bce84665a7c5f519f6c825a5ad7e51512
oiov <stacystayfine718@gmail.com>
2024-04-15 14:46:48 +0800

    fix: chore svg code

--- apps/remix/app/components/Header.tsx
+++ apps/remix/app/components/Header.tsx
@@ -1,7 +1,6 @@
 import { Link } from "@remix-run/react";
 import { VmailLogo } from "./icons/vmail";
 import { useTranslation } from "react-i18next";
-import Follow from "./icons/Follow";
 
 export default function Header() {
   const { t } = useTranslation();
@@ -40,9 +39,9 @@ export default function Header() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className="mb-[1px]">
             <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
           </svg>
