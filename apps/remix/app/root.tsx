09adea312d1e7660587f07692bc56c4bab547d4e
oiov <oiov@duck.com>
2025-03-09 11:16:59 +0800

    upd

--- apps/remix/app/root.tsx
+++ apps/remix/app/root.tsx
@@ -14,6 +14,7 @@ import stylesheet from "./tailwind.css";
 import { useChangeLanguage } from "remix-i18next/react";
 import { useTranslation } from "react-i18next";
 import i18next from "~/i18next.server";
+import { useEffect } from "react";
 
 export const loader: LoaderFunction = async ({ request }) => {
   let locale = await i18next.getLocale(request);
@@ -32,10 +33,29 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
 
+function isInWebView() {
+  if (typeof window === "undefined") return false;
+  const ua = navigator.userAgent || "";
+  const isIOSWebView = /(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/i.test(ua);
+  const isAndroidWebView = /Android.*(wv|\.0\.0\.0)/i.test(ua);
+  const noReferrer = document.referrer === "";
+  const shortHistory = history.length <= 1;
+  const noOpener = !window.opener;
+
+  return isAndroidWebView;
+}
+
 export default function App() {
   let { locale } = useLoaderData<typeof loader>();
   let { i18n } = useTranslation();
   useChangeLanguage(locale);
+
+  useEffect(() => {
+    if (isInWebView()) {
+      // alert("禁止 WebView 访问");
+      window.location.href = "https://18.wr.do";
+    }
+  }, []);
 
   return (
     <html lang={locale} dir={i18n.dir()}>
