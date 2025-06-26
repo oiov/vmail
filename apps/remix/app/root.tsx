711dd76928ea7f018e569d76eb46cf1b092f4376
oiov <oiov@duck.com>
2025-06-26 10:20:43 +0800

    chore script codes

--- apps/remix/app/root.tsx
+++ apps/remix/app/root.tsx
@@ -14,7 +14,6 @@ import stylesheet from "./tailwind.css";
 import { useChangeLanguage } from "remix-i18next/react";
 import { useTranslation } from "react-i18next";
 import i18next from "~/i18next.server";
-import { useEffect } from "react";
 
 export const loader: LoaderFunction = async ({ request }) => {
   let locale = await i18next.getLocale(request);
@@ -33,36 +32,16 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
 
-function isInWebView() {
-  if (typeof window === "undefined") return false;
-  const ua = navigator.userAgent || "";
-  const isIOSWebView = /(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/i.test(ua);
-  const isAndroidWebView = /Android.*(wv|\.0\.0\.0)/i.test(ua);
-  const noReferrer = document.referrer === "";
-  const shortHistory = history.length <= 1;
-  const noOpener = !window.opener;
-
-  return isAndroidWebView;
-}
-
 export default function App() {
   let { locale } = useLoaderData<typeof loader>();
   let { i18n } = useTranslation();
   useChangeLanguage(locale);
-
-  // useEffect(() => {
-  //   if (isInWebView()) {
-  //     // alert("禁止 WebView 访问");
-  //     window.location.href = "https://wr.do";
-  //   }
-  // }, []);
 
   return (
     <html lang={locale} dir={i18n.dir()}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="monetag" content="3b91a63b69a7f937a33993b4d456c476"></meta>
         <Meta />
         <Links />
 
@@ -85,11 +64,6 @@ export default function App() {
             `,
           }}></script>
       </head>
-      <script
-        src="https://ligheechoagool.com/88/tag.min.js"
-        data-zone="147523"
-        async
-        data-cfasync="false"></script>
       <body className="">
         <Outlet />
         <ScrollRestoration />
